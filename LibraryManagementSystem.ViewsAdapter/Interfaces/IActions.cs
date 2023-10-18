49478a41d35d26e06629f4ad02ef7cd6455e228d
Bogumił Rukść <rukscbogumil@gmail.com>
2023-10-18 20:01:48 +0200

    Adding ViewsAdapter project and add all of required nugget packages.

---
+++ LibraryManagementSystem.ViewsAdapter/Interfaces/IActions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.ViewsAdapter.Interfaces
+{
+    public interface IActions
+    {
+        public void Show();
+        public void Hide();
+    }
+}
