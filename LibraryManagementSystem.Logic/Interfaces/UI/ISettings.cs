947174756a616a473958bf9f12c35b2f9b8f7bf9
Bogumił Rukść <rukscbogumil@gmail.com>
2023-11-11 20:13:55 +0100

    Editing namespaces in the logic project interfaces

--- LibraryManagementSystem.Logic/Interfaces/UI/ISettings.cs
+++ LibraryManagementSystem.Logic/Interfaces/UI/ISettings.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace LibraryManagementSystem.Interfaces
+namespace LibraryManagementSystem.Logic.UI.Interfaces
 {
     internal interface ISettings
     {
