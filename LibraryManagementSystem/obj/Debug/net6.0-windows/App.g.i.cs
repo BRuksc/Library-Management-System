1a97992c21e4f43bab9f7af75297c17210a0a435
Bogumił Rukść <bogumil.ruksc@symfonia.pl>
2023-12-19 19:47:29 +0100

    Adding .gitignore

--- LibraryManagementSystem/obj/Debug/net6.0-windows/App.g.i.cs
+++ LibraryManagementSystem/obj/Debug/net6.0-windows/App.g.i.cs
@@ -44,7 +44,7 @@ namespace LibraryManagementSystem {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public void InitializeComponent() {
             
             #line 4 "..\..\..\App.xaml"
@@ -59,7 +59,7 @@ namespace LibraryManagementSystem {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.7.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.13.0")]
         public static void Main() {
             LibraryManagementSystem.App app = new LibraryManagementSystem.App();
             app.InitializeComponent();
